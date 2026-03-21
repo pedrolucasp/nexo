90a378e3e8b3436b9a12b049e6ae1d9ff93a828b
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2026-03-21 17:57:35 -0300

    api: add web server & patch package.json's scripts

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,11 @@
 import { createApp } from "./create-app.js";
 import db from "./db.js";
 
+const port = process.env.PORT || '3000'
 const app = createApp(db);
+
+app.listen(port, () => {
+  console.log(`App running on ${port}`)
+})
 
 export default app;
