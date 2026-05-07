248e1c8b0e0998c95eaa3559ab4d67da59521fe9
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2026-05-06 22:57:01 -0300

    frontend: clean up tamagui refs

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,6 @@
-//import '@tamagui/native/setup-teleport';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { Stack } from 'expo-router';
 import { useColorScheme } from 'react-native'
-import { createTamagui, TamaguiProvider, View } from 'tamagui';
-import { tamaguiConfig } from '../tamagui.config'
 
 import { AuthProvider } from '@/context/AuthContext';
 
@@ -15,22 +12,20 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!} >
-      <ThemeProvider value={DefaultTheme}>
-        <AuthProvider>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="auth"
-              options={{
-                headerShown: false,
-                presentation: 'modal'
-              }}
-            />
-            <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-          </Stack>
-        </AuthProvider>
-      </ThemeProvider>
-    </TamaguiProvider>
+    <ThemeProvider value={DefaultTheme}>
+      <AuthProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="auth"
+            options={{
+              headerShown: false,
+              presentation: 'modal'
+            }}
+          />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
