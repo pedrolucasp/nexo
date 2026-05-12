9491cee07e5a589cb6ccb77be219b7fff62b924e
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2026-05-12 11:57:31 -0300

    api: boot background worker queues
    We might need to place it somewhere else though

--- api/src/createApp.ts
+++ api/src/createApp.ts
@@ -2,13 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import mainRouter from '@app/routes/main';
 import userRouter from '@app/routes/users';
 import authRouter from '@app/routes/auth';
 import { errorHandler } from '@app/middleware/errorHandler';
 import { PrismaClient } from '@prisma/client';
+import { bootWorkers, closeAllWorkers, closeAllQueues } from '@app/lib/queue';
 
 export function createApp() {
   const app = express();
+  bootWorkers();
 
   // Configure CORS
   app.use(cors({
@@ -23,10 +26,19 @@ export function createApp() {
     app.use(morgan('combined'));
   }
 
+  app.get('/', mainRouter);
   app.use("/users", userRouter);
   app.use("/auth", authRouter);
 
   app.use(errorHandler); // always last
 
+  process.on('SIGTERM', shutdown);
+  process.on('SIGINT', shutdown);
+
   return app;
 }
+
+const shutdown = async () => {
+  await Promise.all([closeAllWorkers(), closeAllQueues()]);
+  process.exit(0);
+};
