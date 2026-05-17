ad68b5c88b71e8eee79a79e79609ea7618520328
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2026-05-16 22:25:54 -0300

    api: add file limitations for the avatar service + content disposition

--- api/src/services/avatar.ts
+++ api/src/services/avatar.ts
@@ -5,19 +5,32 @@ import path from 'path';
 import crypto from 'crypto';
 
 const avatarMulter = multerBase({
+  limits: { fileSize: 8 * 1024 * 1024 }, // 8MB
+  fileFilter: (_req, file, cb) => {
+    const allowed = [
+      'image/jpeg',
+      'image/png',
+      'image/webp',
+      'image/jpg'
+    ];
+
+    cb(null, allowed.includes(file.mimetype));
+  },
+
   storage: multerS3({
     s3,
     acl: "public-read",
+    contentDisposition: "inline",
     bucket: process.env.S3_BUCKET!,
-    metadata: function (req, file, cb) {
+    metadata: (req, file, cb) => {
       cb(null, { fieldName: file.fieldname });
     },
-    key: function (req, file, cb) {
+    key: (req, file, cb) => {
       const ext = path.extname(file.originalname).slice(1);
       const uuid = crypto.randomUUID();
 
       cb(null, `avatars/${uuid}.${ext}`)
-    }
+    },
   })
 })
 
