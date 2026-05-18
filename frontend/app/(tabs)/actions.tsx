e4e2bfde523f10abf30a7a1fc1f18f5a49f0ef6b
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2026-05-18 19:38:27 -0300

    frontend: draft actions page

--- frontend/app/(tabs)/actions.tsx
+++ frontend/app/(tabs)/actions.tsx
@@ -1,7 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
 import { ThemedText } from '@/components/misc/themed-text';
 import { ThemedView } from '@/components/misc/themed-view';
 import { ScreenLayout } from '@/components/ui/ScreenLayout';
 import { useAuth } from '@/context/AuthContext';
+import { Section, SectionHeader } from '@/components/ui/Sections';
+import { Card } from '@/components/ui/Cards';
+import { Grid, Col, Row, Between } from '@/components/ui/LayoutHelpers';
+import { Button } from '@/components/ui/Button'
+import { Shadows, Colors, Typography, Spacing, BorderRadius } from '@/constants/theme';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Actions() {
   const { user } = useAuth();
@@ -10,11 +17,90 @@ export default function Actions() {
     <ScreenLayout userName={user.firstName} userAvatar={user.avatarURL}
       onNotificationPress={() => console.log('Notifications')}
       showNotificationBadge={true}>
-      <ThemedView>
-        <ThemedText>
-          Novo
-        </ThemedText>
-      </ThemedView>
+      <Section>
+        <SectionHeader
+          title="Hub de Ações e Gestão"
+          subtitle="Sua ferramenta de apoio para menter a rotina de saúde organizada e monitorada"
+          >
+        </SectionHeader>
+
+        <Button title="Registrar Intervenção" size="large" style={{
+          ...Shadows.lg
+        }} />
+      </Section>
+
+      <Card style={{ padding: Spacing.cardGap, height: 150 }}>
+        <View style={[styles.cardIcon, { backgroundColor: '#E7FDEF' }]}>
+          <Ionicons name="bandage-outline" size={20} color="#13EC5B" />
+        </View>
+
+        <Text style={styles.cardTitleHeadline}>
+          Gerenciar Medicamentos
+        </Text>
+
+        <Text style={styles.cardDescriptionHeadline}>
+          Controle horários, doses e estoque de suas prescrições.
+        </Text>
+      </Card>
+
+      <Grid gap={4}>
+        <Card style={{ padding: Spacing.cardGap }}>
+          <View style={[styles.cardIcon, { backgroundColor: '#EFF6FF' }]}>
+            <Ionicons name="moon-outline" size={20} color="#2563EB" />
+          </View>
+
+          <Text style={styles.cardTitle}>
+            Registrar Sono
+          </Text>
+
+          <Text style={styles.cardDescription}>
+            Informe duração e qualidade do sono.
+          </Text>
+        </Card>
+
+        <Card style={{ padding: Spacing.cardGap }}>
+          <View style={[styles.cardIcon, { backgroundColor: '#FFF7ED' }]}>
+            <Ionicons name="flash-outline" size={20} color="#EA580C" />
+          </View>
+
+          <Text style={styles.cardTitle}>
+            Adicionar Gatilho
+          </Text>
+
+          <Text style={styles.cardDescription}>
+            Registre eventos que aferatam seu humor.
+          </Text>
+        </Card>
+      </Grid>
     </ScreenLayout>
   )
 }
+
+const styles = StyleSheet.create({
+  cardIcon: {
+    paddingVertical: 15,
+    paddingHorizontal: 14,
+    width: 48,
+    borderRadius: BorderRadius.md,
+  },
+  cardTitleHeadline: {
+    ...Typography.headlineMd,
+    lineHeight: 20,
+    marginTop: 4
+  },
+  cardTitle: {
+    ...Typography.bodyLg,
+    lineHeight: 20,
+    marginTop: 4
+  },
+  cardDescriptionHeadline: {
+    ...Typography.bodyMg,
+    color: Colors.light.textSecondary,
+    marginTop: 4
+  },
+  cardDescription: {
+    ...Typography.labelSm,
+    color: Colors.light.textSecondary,
+    marginTop: 4
+  }
+})
