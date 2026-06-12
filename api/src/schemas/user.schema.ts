28956b73c7c5447650edfe10a874c4e61a49cbf0
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2026-06-12 18:51:08 -0300

    api: store user's push token

--- api/src/schemas/user.schema.ts
+++ api/src/schemas/user.schema.ts
@@ -1,17 +1,16 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const CreateUserSchema = z.object({
   firstName: z.string(),
   lastName: z.string().optional(),
   email: z.string().email(),
-  password: z.string().min(6)
+  password: z.string().min(6),
+  pushToken: z.string().optional(),
 });
 
-export const UpdateUserSchema = CreateUserSchema
-  .partial()
-  .extend({
-    id: z.number()
-  });
+export const UpdateUserSchema = CreateUserSchema.partial().extend({
+  id: z.number(),
+});
 
 export type CreateUserInput = z.infer<typeof CreateUserSchema>;
 export type UpdateUserInput = z.infer<typeof UpdateUserSchema>;
