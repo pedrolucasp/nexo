0874e55a0c0ad293a30632ee48f03efcf601bdf9
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2026-06-14 01:55:27 -0300

    api: generate trigger + sleep insights when creating these

--- api/src/services/sleepRecord.service.ts
+++ api/src/services/sleepRecord.service.ts
@@ -2,16 +2,23 @@ import { prisma } from '@app/lib/prisma';
 import { SleepRecord } from '@prisma/client';
 
 import { CreateSleepRecordInput } from '@app/schemas';
+import { rollingPeriod } from "@app/lib/queue/processors/insights/utils";
+import { InsightJobName } from "@app/lib/queue/types";
+import { getQueue } from "@app/lib/queue";
 
 export const createSleepRecord = async (
   userId: number, input: CreateSleepRecordInput
 ): Promise<SleepRecord> => {
-  return prisma.sleepRecord.create({
+  const sleepRecord = await prisma.sleepRecord.create({
     data: {
       ...input,
       userId
     }
   });
+
+  void enqueueEnergySleepInsight(userId);
+
+  return sleepRecord;
 };
 
 export const getSleepRecordsByUserId = async (
@@ -66,4 +73,15 @@ export const destroySleepRecordById = async (
       userId
     }
   })
+}
+
+async function enqueueEnergySleepInsight(userId: number): Promise<void> {
+  await getQueue('insights').add(
+    InsightJobName.EnergySleep,
+    { userId, ...rollingPeriod(7) },
+    {
+      jobId: `energy-sleep-${userId}-${new Date().toDateString()}`,
+      removeOnComplete: true,
+    }
+  );
 }
--- api/src/services/trigger.service.ts
+++ api/src/services/trigger.service.ts
@@ -2,16 +2,23 @@ import { prisma } from '@app/lib/prisma';
 import { Trigger } from '@prisma/client';
 
 import { CreateTriggerInput } from '@app/schemas';
+import { rollingPeriod } from "@app/lib/queue/processors/insights/utils";
+import { InsightJobName } from "@app/lib/queue/types";
+import { getQueue } from "@app/lib/queue";
 
 export const createTrigger = async (
   userId: number, input: CreateTriggerInput
 ): Promise<Trigger> => {
-  return prisma.trigger.create({
+  const trigger = await prisma.trigger.create({
     data: {
       ...input,
       userId
     }
   });
+
+  void enqueueTriggerPatternInsight(userId);
+
+  return trigger
 };
 
 export const getTriggersByUserId = async (
@@ -66,4 +73,15 @@ export const destroyTriggerById = async (
       userId
     }
   })
+}
+
+async function enqueueTriggerPatternInsight(userId: number): Promise<void> {
+  await getQueue('insights').add(
+    InsightJobName.TriggerPattern,
+    { userId, ...rollingPeriod(7) },
+    {
+      jobId: `trigger-pattern-${userId}-${new Date().toDateString()}`,
+      removeOnComplete: true,
+    }
+  );
 }
